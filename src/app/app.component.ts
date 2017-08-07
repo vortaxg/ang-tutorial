590329330c6b2fac138edea9e6eb5f9e4d50b07f
Vitalii <vortaxg@gmail.com>
2017-08-07 17:48:02 +0300

    temp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+            <h1>{{title}}</h1>
+            <h2>{{hero.name}} details!</h2>
+            <div><label>id: </label>{{hero.id}}</div>
+            <div>
+                <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name"/>
+            </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  hero: Hero;
+  constructor() {
+    this.title = 'Tour of Heroes';
+    this.hero = {
+      id: 1,
+      name: 'Windstorm'
+    };
+  }
+}
+export class Hero {
+  id: number;
+  name: string;
 }
