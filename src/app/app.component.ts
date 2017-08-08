31e8b743178cd2419ccd6cf2d4a459d1f38525bd
Vitalii <vortaxg@gmail.com>
2017-08-08 15:58:08 +0300

    added user select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,36 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
-  template: `
-            <h1>{{title}}</h1>
-            <h2>{{hero.name}} details!</h2>
-            <div><label>id: </label>{{hero.id}}</div>
-            <div>
-                <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name"/>
-            </div>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title: string;
-  hero: Hero;
+  heroes: Hero[];
+  selectedHero: Hero;
   constructor() {
     this.title = 'Tour of Heroes';
-    this.hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
+    this.heroes = HEROES;
   }
-}
-export class Hero {
-  id: number;
-  name: string;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
